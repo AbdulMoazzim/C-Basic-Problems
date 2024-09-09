f248df27d5c97a0db8b934d540e441d0cb2df81e
Abdul Moazzim <abdulmoazzam0815@gmail.com>
2024-09-09 16:57:47 +0500

    Made two files

---
+++ Prime Numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//Checking Prime Numbers
+int primeChecking(int n){
+    int count = 0;
+    for (int i = 1; i <= n; i++){
+        if (n % i == 0){
+            count++;
+        }
+    }
+    if (count == 2) {
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+int main(){
+    printf("%d\n", primeChecking(6));
+}
---
+++ Swapping Two Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//swapping Two Numbers
+void swap(int* a, int* b) {
+   int temp = *a;
+   *a = *b;
+   *b = temp;
+}
+int main(){
+    int a = 5; 
+    int b = 6;
+    printf("%d %d\n", swap(&a,&b));
+}
